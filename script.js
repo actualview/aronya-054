7b223a16e18a198076b310c9bfa2318c086e9c07
actualview <138713344+actualview@users.noreply.github.com>
2023-07-25 16:55:19 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Whale',
+            location: {
+                lat: 35.147650,
+                lng: 129.124915,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${35.147650}; longitude: ${129.124915};`);
+        model.setAttribute('gltf-model', './assets/whale/scene.glb');
+        model.setAttribute('rotation', '0 30 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.005 0.005 0.005');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
